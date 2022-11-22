122aa46c40df5563da8ecbb72acaef7087bb8ac4
teldufalsari <idunaev71@gmail.com>
2022-11-23 00:47:00 +0300

    Add solution to 10-ext2-fuse

---
+++ 10-ext2-fuse/ext2wrappers.c
@@ -0,0 +1,606 @@
+#include "ext2wrappers.h"
+
+//~~~~~~~~~~~~~~~~~~~~~~~ UTILS ~~~~~~~~~~~~~~~~~~~~~~~//
+
+ssize_t pread_exact(int fd, void *buf, size_t count, off_t offset)
+{
+	ssize_t actually_read = 0;
+	while ((size_t)actually_read < count) {
+		ssize_t current_read =
+		    pread(fd, buf, count, offset + actually_read);
+		if (current_read > 0) {
+			actually_read += current_read;
+			count -= current_read;
+		} else if (errno == EINTR) {
+			continue;
+		} else if (current_read == 0) {
+			break;
+		} else {
+			return -1;
+		}
+	}
+	return actually_read;
+}
+
+ssize_t write_exact(int fd, const void *buf, size_t count)
+{
+	ssize_t actually_written = 0;
+	while ((size_t)actually_written < count) {
+		ssize_t current_write = write(fd, buf, count);
+		if (current_write > 0) {
+			actually_written += current_write;
+			count -= current_write;
+		} else if (errno == EINTR) {
+			continue;
+		} else if (current_write == 0) {
+			break;
+		} else {
+			return -1;
+		}
+	}
+	return actually_written;
+}
+
+ssize_t load_inode(
+	int imgfd, unsigned inode_nr, 
+	const struct ext2_super_block *sb, struct ext2_inode *buf)
+{
+	struct ext2_group_desc bg_header = { };
+	unsigned block_size = get_ext2_block_size(sb->s_log_block_size);
+	unsigned bg_number = (inode_nr - 1) / sb->s_inodes_per_group;
+	unsigned inode_index = (inode_nr - 1) % sb->s_inodes_per_group;
+	/*
+	 * Read the block group header and get the inode table offset. 
+	 */
+	off_t bg_header_offset =
+	    SUPERBLOCK_OFFSET + sizeof(*sb) + bg_number * sizeof(bg_header);
+	ssize_t read_size = pread_exact(imgfd, &bg_header, sizeof(bg_header),
+					bg_header_offset);
+	if (read_size < 0)
+		return -errno;
+	off_t inode_offset =
+	    block_size * bg_header.bg_inode_table +
+	    inode_index * sb->s_inode_size;
+	/*
+	 * Read the inode 
+	 */
+	return pread_exact(imgfd, buf, sizeof(*buf), inode_offset);
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~ PATH TRAVERSE ~~~~~~~~~~~~~~~~~~~~~~~//
+
+int get_inode_nr(int img, const char* path, const struct ext2_super_block* sb)
+{
+	if (strcmp(path, "/") == 0) {
+		return 2;
+	} else {
+		// Make a copy of the path
+		// It will be modified in a strtok() fashion
+		char* path_copy = strdup(path);
+		int inode_nr = find_inode_by_path(img, 2, path_copy + 1, sb);
+		free(path_copy);
+		return inode_nr;
+	}
+}
+
+int find_inode_by_path(
+	int imgfd, int start_inode_nr,
+	char *path, const struct ext2_super_block *sb)
+{
+	int ret;
+	struct ext2_inode inode = { };
+	size_t block_size = get_ext2_block_size(sb->s_log_block_size);
+	if ((ret = load_inode(imgfd, start_inode_nr, sb, &inode)) < 0)
+		return ret;
+	char *next_path_part = strchr(path, '/');
+	if (next_path_part == NULL) {
+		// we've reached the final part of the path
+		// return file inode number
+		return find_entry_inode(imgfd, EXT2_FT_UNKNOWN, path, block_size, &inode);
+	} else {
+		*(next_path_part++) = '\0';	// step over '/' symbol
+		int next_nr =
+		    find_entry_inode(imgfd, EXT2_FT_DIR, path, block_size, &inode);
+		if (next_nr < 0) {
+			return next_nr;
+		} else {
+			// Let's hope this will be a tail recursive call
+			return find_inode_by_path(imgfd, next_nr, next_path_part, sb);
+		}
+	}
+}
+
+int find_entry_inode(
+	int imgfd, int type, const char *name, 
+	size_t block_size, const struct ext2_inode *inode)
+{
+	char *buf = malloc(block_size);
+	int inode_nr = 0;
+	for (unsigned i = 0; i < EXT2_NDIR_BLOCKS; i++) {
+		if (inode->i_block[i] == 0) {
+			free(buf);
+			return -ENOENT;
+		}
+		if (load_block(imgfd, inode->i_block[i], buf, block_size) < 0) {
+			free(buf);
+			return -errno;
+		}
+		inode_nr =
+		    find_record_in_dir_block(buf, name, block_size, type);
+		/*
+		 * After this call {
+		 *     inode_nr > 0 => record successfully found -- return record inode number,
+		 *     inode_nr == 0 => record not found -- continue,
+		 *     inode_nr < 0 => an error occured -- return error code
+		 * }; 
+		 */
+		if (inode_nr != 0) {
+			free(buf);
+			return inode_nr;
+		}
+	}
+	if (inode->i_block[EXT2_IND_BLOCK] != 0) {
+		if (load_block(imgfd, inode->i_block[EXT2_IND_BLOCK], buf, block_size) < 0) {
+			free(buf);
+			return -errno;
+		}
+		if ((inode_nr = find_record_in_indir_block(
+				imgfd, type, (unsigned *)buf, name, block_size)) != 0) {
+			free(buf);
+			return inode_nr;
+		}
+	}
+	if (inode->i_block[EXT2_DIND_BLOCK] != 0) {
+		if (load_block(imgfd, inode->i_block[EXT2_DIND_BLOCK], buf, block_size) < 0) {
+			free(buf);
+			return -errno;
+		}
+		if ((inode_nr = find_record_in_dindir_block(
+				imgfd, type, (unsigned *)buf, name, block_size)) != 0) {
+			free(buf);
+			return inode_nr;
+		}
+	}
+	free(buf);
+	return -ENOENT;
+}
+
+int find_record_in_dir_block(
+	const char *buf, const char *name, size_t block_size, int type)
+{
+	size_t offset = 0;
+	while (offset + sizeof(struct ext2_dir_entry) - EXT2_NAME_LEN < block_size) {
+		const struct ext2_dir_entry *entry = (const struct ext2_dir_entry *)(buf + offset);
+		if (entry->rec_len == 0) {	// no more records in this block
+			return 0;
+		} else if (entry->inode == 0) {	// reserved empty space
+			offset += entry->rec_len;
+			continue;
+		}
+		// normal entry:
+		unsigned name_len = (unsigned)ext2fs_dirent_name_len(entry);
+		if (name_len == strlen(name)) {
+			if (strncmp(name, entry->name, name_len) == 0) {
+				// this is the entry we are looking for
+				if ((type == EXT2_FT_DIR) && (type != ext2fs_dirent_file_type(entry)))
+					return -ENOTDIR;
+				else
+					return entry->inode;
+			}
+		}
+		// This is not the entry we are looking for - skip it
+		offset += entry->rec_len;
+	}
+	return 0;
+}
+
+int find_record_in_indir_block(
+	int imgfd, int type, const unsigned *indir_block, 
+	const char *name, size_t block_size)
+{
+	char *buf = malloc(block_size);
+	unsigned block_ref_count = block_size / sizeof(unsigned);
+	int inode_nr = 0;
+	for (unsigned i = 0; i < block_ref_count; i++) {
+		if (indir_block[i] == 0) {
+			free(buf);
+			return 0;
+		}
+		if (load_block(imgfd, indir_block[i], buf, block_size) < 0) {
+			free(buf);
+			return -errno;
+		}
+		if ((inode_nr = find_record_in_dir_block(
+				buf, name, block_size, type)) != 0) {
+			free(buf);
+			return inode_nr;
+		}
+	}
+	free(buf);
+	return 0;
+}
+
+int find_record_in_dindir_block(
+	int imgfd, int type, const unsigned *dindir_block,
+	const char *name, size_t block_size)
+{
+	unsigned *buf = malloc(block_size);
+	unsigned block_ref_count = block_size / sizeof(unsigned);
+	int inode_nr = 0;
+	for (unsigned i = 0; i < block_ref_count; i++) {
+		if (dindir_block[i] == 0) {
+			free(buf);
+			return 0;
+		}
+		if (load_block(imgfd, dindir_block[i], (char *)buf, block_size) < 0) {
+			free(buf);
+			return -errno;
+		}
+		if ((inode_nr = find_record_in_indir_block(
+				imgfd, type, buf, name, block_size)) != 0) {
+			free(buf);
+			return inode_nr;
+		}
+	}
+	free(buf);
+	return 0;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~ READDIR ~~~~~~~~~~~~~~~~~~~~~~~//
+
+void parse_directory_block(
+	const char *buf, size_t block_size, 
+	void *fuse_buf, fuse_fill_dir_t fill_callback)
+{
+	size_t offset = 0;
+	char name[256] = {};
+	while (offset + sizeof(struct ext2_dir_entry) - EXT2_NAME_LEN < block_size) {
+		const struct ext2_dir_entry *entry = (const struct ext2_dir_entry *)(buf + offset);
+		if (entry->rec_len == 0) { // no more records in this block
+			break;
+		} else if (entry->inode == 0) {	// reserved empty space
+			offset += entry->rec_len;
+			continue;
+		}
+		// normal entry
+		// get name in readable format
+		int name_len = ext2fs_dirent_name_len(entry);
+		memcpy(name, entry->name, name_len);
+		name[name_len] = '\0';
+		// report
+		fill_callback(fuse_buf, name, NULL, 0, 0);
+		// load next
+		offset += entry->rec_len;
+	}
+}
+
+int parse_indirect_block(
+	int imgfd, const unsigned *indir_block, size_t block_size,
+	void *fuse_buf, fuse_fill_dir_t fill_callback)
+{
+	char *buf = malloc(block_size);
+	unsigned block_ref_count = block_size / sizeof(unsigned);
+	for (unsigned i = 0; i < block_ref_count; i++) {
+		if (indir_block[i] == 0)
+			break;
+		if (load_block(imgfd, indir_block[i], buf, block_size) < 0) {
+			free(buf);
+			return -errno;
+		}
+		parse_directory_block(buf, block_size, fuse_buf, fill_callback);
+	}
+	free(buf);
+	return 0;
+}
+
+int parse_double_indirect_block(
+	int imgfd, const unsigned *dindir_block, 
+	size_t block_size, void *fuse_buf, fuse_fill_dir_t fill_callback)
+{
+	unsigned *buf = malloc(block_size);
+	unsigned block_ref_count = block_size / sizeof(unsigned);
+	for (unsigned i = 0; i < block_ref_count; i++) {
+		if (dindir_block[i] == 0)
+			break;
+		if (load_block(imgfd, dindir_block[i], (char *)buf, block_size) < 0) {
+			free(buf);
+			return -errno;
+		}
+		parse_indirect_block(imgfd, buf, block_size, fuse_buf, fill_callback);
+	}
+	free(buf);
+	return 0;
+}
+
+int parse_dir_by_inode(
+	int img, size_t block_size, const struct ext2_inode *inode_ptr,
+	void *fuse_buf, fuse_fill_dir_t fill_callback)
+{
+	char *buf = malloc(block_size);
+	for (unsigned i = 0; i < EXT2_NDIR_BLOCKS; i++) {
+		if (inode_ptr->i_block[i] == 0)
+			break;
+		if (load_block(img, inode_ptr->i_block[i], buf, block_size) < 0) {
+			free(buf);
+			return -errno;
+		}
+		parse_directory_block(buf, block_size, fuse_buf, fill_callback);
+	}
+	if (inode_ptr->i_block[EXT2_IND_BLOCK] != 0) {
+		if (load_block (img, inode_ptr->i_block[EXT2_IND_BLOCK], buf, block_size) < 0) {
+			free(buf);
+			return -errno;
+		}
+		if (parse_indirect_block
+		    (img, (unsigned *)buf, block_size, fuse_buf, fill_callback) < 0) {
+			free(buf);
+			return -errno;
+		}
+	}
+	if (inode_ptr->i_block[EXT2_DIND_BLOCK] != 0) {
+		if (load_block (img, inode_ptr->i_block[EXT2_DIND_BLOCK], buf, block_size) < 0) {
+			free(buf);
+			return -errno;
+		}
+		if (parse_double_indirect_block
+		    (img, (unsigned *)buf, block_size, fuse_buf, fill_callback) < 0) {
+			free(buf);
+			return -errno;
+		}
+	}
+	free(buf);
+	return 0;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~ READ ~~~~~~~~~~~~~~~~~~~~~~~//
+
+int read_indirect(
+	int imgfd, unsigned offset, unsigned size, 
+	unsigned block_size, char *read_buf, unsigned *indir_block)
+{
+	char *buf = malloc(block_size);
+	int ret = 0;
+	unsigned block_ref_count = block_size / sizeof(unsigned);
+	unsigned size_left = size;
+	unsigned start_i = offset / block_size;
+
+	// Deal with the first, uneven block:
+	offset %= block_size;
+	if (start_i < block_ref_count) { // if, just in case
+		unsigned transfer_size = (
+			size_left < (block_size - offset) ?
+			size_left : (block_size - offset));
+		if (indir_block[start_i] != 0) {
+			ret = pread_exact(
+				imgfd, read_buf, transfer_size, 
+				indir_block[start_i] * block_size + offset);
+		} else {
+			memset(read_buf, 0, block_size - offset);
+		}
+		if (ret < 0) {
+			free(buf);
+			return ret;
+		} else {
+			size_left -= transfer_size;
+			read_buf += transfer_size;
+		}
+	}
+
+	for (unsigned i = start_i + 1; (i < block_ref_count) && (size_left > 0); i++) {
+		unsigned transfer_size = (size_left < block_size ? size_left : block_size);
+		if (indir_block[i] != 0) {
+			ret = pread_exact(imgfd, read_buf, transfer_size, indir_block[i] * block_size);
+		} else {
+			memset(read_buf, 0, transfer_size);
+		}
+		if (ret < 0) {
+			free(buf);
+			return ret;
+		} else {
+			size_left -= transfer_size;
+			read_buf += transfer_size;
+		}
+	}
+	free(buf);
+	return (int)(size - size_left);
+}
+
+int read_double_indirect(
+	int imgfd, unsigned offset, unsigned size, 
+	unsigned block_size, char *read_buf,unsigned *dindir_block)
+{
+	unsigned *indirect_block_buf = malloc(block_size);
+	int ret = 0;
+	unsigned block_ref_count = block_size / sizeof(unsigned);
+	unsigned max_size_ref_by_indir = block_ref_count * block_size;
+	unsigned size_left = size;
+	unsigned start_i = offset / max_size_ref_by_indir;
+
+	// Deal with the first, uneven block
+	offset %= max_size_ref_by_indir;	// this is offset inside an
+	// indirect block, in bytes
+	if (start_i < block_ref_count) {
+		if (dindir_block[start_i] == 0) { // a hole
+			unsigned transfer_size = (
+				size_left < (max_size_ref_by_indir - offset) ?
+				size_left : (max_size_ref_by_indir -offset));
+			memset(read_buf, 0, transfer_size);
+			size_left -= transfer_size;
+			read_buf += transfer_size;
+		} else { // a regular block
+			ret = pread_exact(
+				imgfd, indirect_block_buf, block_size,
+				block_size * dindir_block[start_i]);
+			if (ret > 0)
+				ret = read_indirect(
+					imgfd, offset, size_left, block_size,
+					read_buf, indirect_block_buf);
+			if (ret < 0) {
+				free(indirect_block_buf);
+				return ret;
+			} else {
+				size_left -= ret;
+				read_buf += ret;
+			}
+		}
+	}
+
+	// Read remaining blocks
+	for (unsigned i = start_i + 1;
+	     (i < block_ref_count) && (size_left > 0); i++) {
+		if (dindir_block[i] == 0) { // a hole
+			unsigned transfer_size = (
+				size_left < max_size_ref_by_indir ?
+				size_left : max_size_ref_by_indir);
+			memset(read_buf, 0, transfer_size);
+			size_left -= transfer_size;
+			read_buf += transfer_size;
+		} else { // a regular block
+			ret = pread_exact(imgfd, indirect_block_buf, block_size, block_size * dindir_block[i]);
+			if (ret > 0)
+				// copy_indirect returns copied size
+				ret = read_indirect(imgfd, 0, size_left, block_size, read_buf, indirect_block_buf);
+			if (ret < 0) {
+				free(indirect_block_buf);
+				return ret;
+			} else {
+				size_left -= ret;
+				read_buf += ret;
+			}
+		}
+	}
+	free(indirect_block_buf);
+	return 0;
+}
+
+int read_by_inode(
+	int imgfd, const struct ext2_inode *inode_ptr,
+	unsigned block_size, char *read_buf, off_t offset, size_t buf_size)
+{
+	// deal with situations when EOF is reached when reading or when
+	// offset is past EOF
+	if (offset > inode_ptr->i_size) {
+		// return all null
+		memset(read_buf, 0, buf_size);
+		return buf_size;
+	} else if (buf_size > (size_t)(inode_ptr->i_size - offset)) {
+		buf_size = inode_ptr->i_size - offset;
+	}
+	// from now assume buf_size > 0
+	unsigned size_left = buf_size;
+	int ret = 0;
+	char *buf = malloc(block_size);
+	unsigned block_ref_count = block_size / sizeof(unsigned);
+	unsigned max_size_ref_by_indir = block_ref_count * block_size;
+	unsigned start_i = 0;
+
+	// Find where to start
+	if (offset < EXT2_NDIR_BLOCKS * block_size) {
+		// start reading from a direct block
+		start_i = offset / block_size;
+		offset %= block_size;
+		goto read_direct;
+	}
+	// skip all direct blocks
+	offset -= EXT2_NDIR_BLOCKS * block_size;
+	if (offset < max_size_ref_by_indir) {
+		start_i = EXT2_IND_BLOCK;
+		offset %= max_size_ref_by_indir;
+		goto read_indirect;
+	} else {
+		start_i = EXT2_DIND_BLOCK;
+		offset -= max_size_ref_by_indir;
+		goto read_double_indirect;
+	}
+
+ read_direct:
+	// Copy all data in direct blocks
+	// Deal with the first, uneven block
+	unsigned transfer_size = (
+		size_left < (block_size - offset) ?
+		size_left : (block_size - offset));
+	if (inode_ptr->i_block[start_i] == 0) {
+		memset(read_buf, 0, block_size - offset);
+	} else {
+		ret = pread_exact(
+			imgfd, read_buf, transfer_size,
+			inode_ptr->i_block[start_i] * block_size + offset);
+	}
+	if (ret < 0) {
+		free(buf);
+		return ret;
+	} else {
+		size_left -= transfer_size;
+		read_buf += transfer_size;
+	}
+	// Copy even blocks with zero offset
+	for (unsigned i = start_i + 1; (i < EXT2_NDIR_BLOCKS) && (size_left > 0); i++) {
+		unsigned transfer_size = (
+			size_left < block_size ?
+			size_left : block_size);
+		if (inode_ptr->i_block[i] == 0) {	// a hole
+			memset(read_buf, 0, transfer_size);
+		} else {
+			ret = pread_exact(imgfd, read_buf, transfer_size, inode_ptr->i_block[i]);
+		}
+		if (ret < 0) {
+			free(buf);
+			return ret;
+		}
+		size_left -= transfer_size;
+		read_buf += transfer_size;
+	}
+
+	if (offset != 0)
+		offset = 0;
+ read_indirect:
+	// If anything left, copy data from blocks pointed by the indirect
+	// block
+	if (size_left > 0) {
+		if (inode_ptr->i_block[EXT2_IND_BLOCK] == 0) {	// A hole
+			unsigned transfer_size = (
+				size_left < (max_size_ref_by_indir - offset) ?
+				size_left : (max_size_ref_by_indir - offset));
+			memset(read_buf, 0, transfer_size);
+			size_left -= transfer_size;
+			read_buf += transfer_size;
+		} else {
+			pread_exact(
+				imgfd, buf, block_size,
+				block_size * inode_ptr->i_block[EXT2_IND_BLOCK]);
+			// copy_indirect returns copied size
+			ret = read_indirect(
+				imgfd, offset, size_left,block_size, read_buf, (unsigned*)buf);
+			if (ret > 0) {
+				size_left -= ret;
+				read_buf += ret;
+			} else {
+				free(buf);
+				return ret;
+			}
+		}
+	}
+
+	if (offset != 0)
+		offset = 0;
+ read_double_indirect:
+	// If anything left, copy data from blocks pointed by the double
+	// indirect block
+	if (size_left > 0) {
+		if (inode_ptr->i_block[EXT2_DIND_BLOCK] == 0) {	// File has a
+			// "tail" of zeros
+			memset(read_buf, 0, size_left);
+		} else {
+			pread_exact(
+				imgfd, buf, block_size, 
+				block_size * inode_ptr->i_block[EXT2_DIND_BLOCK]);
+			ret = read_double_indirect(
+				imgfd, offset, size_left, block_size, read_buf, (unsigned*)buf);
+		}
+	}
+	// Triple indirect blocks are not supported, we've done all we could
+	free(buf);
+	if (ret < 0)
+		return ret;
+	return buf_size;
+}
--- 10-ext2-fuse/solution.c
+++ 10-ext2-fuse/solution.c
@@ -1,14 +1,275 @@
 #include <solution.h>
 
 #include <fuse.h>
+#include <string.h>
+#include <errno.h>
+
+#include "ext2wrappers.h"
+
+int g_img = 0;
+unsigned g_block_size = 0;
+struct ext2_super_block g_sb = {};
+
+
+/** Get file attributes.
+ *
+ * Similar to stat().  The 'st_dev' and 'st_blksize' fields are
+ * ignored.	 The 'st_ino' field is ignored except if the 'use_ino'
+ * mount option is given.
+ */
+int ext2_getattr(const char* path, struct stat* stat, struct fuse_file_info* info)
+{
+	(void)info;
+	struct ext2_inode inode = {};
+
+	int inode_nr = get_inode_nr(g_img, path, &g_sb);
+	if (inode_nr < 0)
+		return inode_nr;
+
+	ssize_t ret = 0;
+	if ((ret = load_inode(g_img, inode_nr, &g_sb, &inode)) < 0)
+		return ret;
+
+	struct fuse_context* context = fuse_get_context();
+	stat->st_nlink = inode.i_links_count;
+	stat->st_mode = inode.i_mode;
+	stat->st_uid = context->uid;
+	stat->st_gid = context->gid;
+	stat->st_size = inode.i_size;
+	stat->st_atim.tv_sec = inode.i_atime;
+	stat->st_atim.tv_nsec = 0;
+	stat->st_mtim.tv_sec = inode.i_mtime;
+	stat->st_mtim.tv_nsec = 0;
+	stat->st_ctim.tv_sec = inode.i_ctime;
+	stat->st_ctim.tv_nsec = 0;
+	stat->st_blksize = g_block_size;
+	stat->st_blocks = inode.i_blocks;
+	return 0;
+}
+
+/** Create a file node
+ *
+ * This is called for creation of all non-directory, non-symlink
+ * nodes.  If the filesystem defines a create() method, then for
+ * regular files that will be called instead.
+ */
+int ext2_mknod(const char* path, mode_t mode, dev_t device)
+{
+	(void)path;
+	(void)mode;
+	(void)device;
+	return -EROFS;
+}
+
+/** Create a directory 
+ *
+ * Note that the mode argument may not have the type specification
+ * bits set, i.e. S_ISDIR(mode) can be false.  To obtain the
+ * correct directory type bits use  mode|S_IFDIR
+ * */
+int ext2_mkdir(const char* path, mode_t mode)
+{
+	(void)path;
+	(void)mode;
+	return -EROFS;
+}
+
+/** File open operation
+ *
+ * No creation (O_CREAT, O_EXCL) and by default also no
+ * truncation (O_TRUNC) flags will be passed to open(). If an
+ * application specifies O_TRUNC, fuse first calls truncate()
+ * and then open(). Only if 'atomic_o_trunc' has been
+ * specified and kernel version is 2.6.24 or later, O_TRUNC is
+ * passed on to open.
+ *
+ * Unless the 'default_permissions' mount option is given,
+ * open should check if the operation is permitted for the
+ * given flags. Optionally open may also return an arbitrary
+ * filehandle in the fuse_file_info structure, which will be
+ * passed to all file operations.
+ *
+ * Changed in version 2.2
+ */
+int ext2_open(const char* path, struct fuse_file_info* info)
+{
+	struct ext2_inode inode = {};
+	int inode_nr = get_inode_nr(g_img, path, &g_sb);
+	if (inode_nr < 0)
+		return inode_nr;
+
+	ssize_t ret = 0;
+	if ((ret = load_inode(g_img, inode_nr, &g_sb, &inode)) < 0)
+		return ret;
+	
+	if (((inode.i_mode & S_IFMT) == S_IFDIR) && ((info->flags & O_DIRECTORY) == 0)) {
+		return -EISDIR;
+	} else if (info->flags & (O_WRONLY | O_RDWR)) {
+		return -EROFS;
+	} else {
+		// Do I need to check permissions?
+		return 0;
+	}
+}
+
+/** Read data from an open file
+ *
+ * Read should return exactly the number of bytes requested except
+ * on EOF or error, otherwise the rest of the data will be
+ * substituted with zeroes.	 An exception to this is when the
+ * 'direct_io' mount option is specified, in which case the return
+ * value of the read system call will reflect the return value of
+ * this operation.
+ *
+ * Changed in version 2.2
+ */
+int ext2_read(const char* path, char* buf, 
+	size_t buf_size, off_t offset, struct fuse_file_info* info)
+{
+	(void)info;
+	struct ext2_inode inode = {};
+	int inode_nr = get_inode_nr(g_img, path, &g_sb);
+	if (inode_nr < 0)
+		return inode_nr;
+
+	ssize_t ret = 0;
+	if ((ret = load_inode(g_img, inode_nr, &g_sb, &inode)) < 0)
+		return ret;
+	ret = read_by_inode(g_img, &inode, g_block_size, buf, offset, buf_size);
+	return ret;
+}
+
+/** Write data to an open file
+ *
+ * Write should return exactly the number of bytes requested
+ * except on error.	 An exception to this is when the 'direct_io'
+ * mount option is specified (see read operation).
+ *
+ * Changed in version 2.2
+ */
+int ext2_write(const char* path, const char* buf,
+	size_t buf_size, off_t offset, struct fuse_file_info* info)
+{
+	(void)buf;
+	(void)buf_size;
+	(void)offset;
+	(void)info;
+
+	int inode_nr = get_inode_nr(g_img, path, &g_sb);
+
+	if (inode_nr < 0) {
+		return inode_nr;
+	} else {
+		return -EROFS;
+	}
+}
+
+/** Open directory
+ *
+ * Unless the 'default_permissions' mount option is given,
+ * this method should check if opendir is permitted for this
+ * directory. Optionally opendir may also return an arbitrary
+ * filehandle in the fuse_file_info structure, which will be
+ * passed to readdir, releasedir and fsyncdir.
+ *
+ * Introduced in version 2.3
+ */
+int ext2_opendir(const char* path, struct fuse_file_info* info)
+{
+	(void)info;
+	struct ext2_inode inode;
+
+	int inode_nr = get_inode_nr(g_img, path, &g_sb);
+	if (inode_nr < 0)
+		return inode_nr;
+	
+	ssize_t ret = 0;
+	if ((ret = load_inode(g_img, inode_nr, &g_sb, &inode)) < 0) {
+		return ret;
+	}
+
+	if ((inode.i_mode & S_IFMT) != S_IFDIR) {
+		return -ENOTDIR;
+	} else {
+		// Do I need to check permissions?
+		return 0;
+	}
+}
+
+/** Read directory
+ *
+ * This supersedes the old getdir() interface.  New applications
+ * should use this.
+ *
+ * The filesystem may choose between two modes of operation:
+ *
+ * 1) The readdir implementation ignores the offset parameter, and
+ * passes zero to the filler function's offset.  The filler
+ * function will not return '1' (unless an error happens), so the
+ * whole directory is read in a single readdir operation.  This
+ * works just like the old getdir() method.
+ *
+ * 2) The readdir implementation keeps track of the offsets of the
+ * directory entries.  It uses the offset parameter and always
+ * passes non-zero offset to the filler function.  When the buffer
+ * is full (or an error happens) the filler function will return
+ * '1'.
+ *
+ * Introduced in version 2.3
+ */
+int ext2_readdir(const char* path, void* buf, fuse_fill_dir_t fill_callback,
+	off_t offset, struct fuse_file_info* info, enum fuse_readdir_flags flags)
+{
+	(void)offset;
+	(void)info;
+	(void)flags;
+
+	struct ext2_inode inode = {};
+	int inode_nr = get_inode_nr(g_img, path, &g_sb);
+	if (inode_nr < 0)
+		return inode_nr;
+
+	ssize_t ret = 0;
+	if ((ret = load_inode(g_img, inode_nr, &g_sb, &inode)) < 0)
+		return ret;
+	return parse_dir_by_inode(g_img, g_block_size, &inode, buf, fill_callback);
+}
+
+/**
+ * Initialize filesystem
+ *
+ * The return value will passed in the private_data field of
+ * fuse_context to all file operations and as a parameter to the
+ * destroy() method.
+ *
+ * Introduced in version 2.3
+ * Changed in version 2.6
+ */
+void* ext2_init(struct fuse_conn_info* conn, struct fuse_config* config)
+{
+	(void)conn;
+	(void)config;
+	return NULL;
+}
 
 static const struct fuse_operations ext2_ops = {
-	/* implement me */
+	.getattr = &ext2_getattr,
+	.mknod = &ext2_mknod,
+	.mkdir = &ext2_mkdir,
+	.open = &ext2_open,
+	.read = &ext2_read,
+	.write = &ext2_write,
+	.opendir = &ext2_opendir,
+	.readdir = &ext2_readdir,
+	.init = &ext2_init,
 };
 
 int ext2fuse(int img, const char *mntp)
 {
-	(void) img;
+	g_img = img;
+	if (pread_exact(img, &g_sb, sizeof(g_sb), SUPERBLOCK_OFFSET) < 0)
+		return -errno;
+	g_block_size = get_ext2_block_size(g_sb.s_log_block_size);
 
 	char *argv[] = {"exercise", "-f", (char *)mntp, NULL};
 	return fuse_main(3, argv, &ext2_ops, NULL);
