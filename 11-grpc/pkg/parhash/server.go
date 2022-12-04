bfd8541e8e4a8ce24d9adb31dafa312785280d66
teldufalsari <idunaev71@gmail.com>
2022-12-04 22:54:47 +0300

    add solution to 11-grpc

--- 11-grpc/pkg/parhash/server.go
+++ 11-grpc/pkg/parhash/server.go
@@ -2,9 +2,17 @@ package parhash
 
 import (
 	"context"
+	"log"
+	"net"
+	"sync"
 
 	"github.com/pkg/errors"
 	"golang.org/x/sync/semaphore"
+	"google.golang.org/grpc"
+
+	hashpb "fs101ex/pkg/gen/hashsvc"
+	parhashpb "fs101ex/pkg/gen/parhashsvc"
+	"fs101ex/pkg/workgroup"
 )
 
 type Config struct {
@@ -38,29 +46,89 @@ type Config struct {
 type Server struct {
 	conf Config
 
-	sem *semaphore.Weighted
+	sem     *semaphore.Weighted
+	lock    sync.Mutex
+	current int
+
+	stop context.CancelFunc
+	l    net.Listener
+	wg   sync.WaitGroup
 }
 
 func New(conf Config) *Server {
 	return &Server{
-		conf: conf,
-		sem:  semaphore.NewWeighted(int64(conf.Concurrency)),
+		conf:    conf,
+		sem:     semaphore.NewWeighted(int64(conf.Concurrency)),
+		current: 0,
 	}
 }
 
 func (s *Server) Start(ctx context.Context) (err error) {
 	defer func() { err = errors.Wrap(err, "Start()") }()
 
-	/* implement me */
+	ctx, s.stop = context.WithCancel(ctx)
+	s.l, err = net.Listen("tcp", s.conf.ListenAddr)
+	if err != nil {
+		return err
+	}
+	srv := grpc.NewServer()
+	parhashpb.RegisterParallelHashSvcServer(srv, s)
 
+	s.wg.Add(2)
+	go func() {
+		defer s.wg.Done()
+		srv.Serve(s.l)
+	}()
+	go func() {
+		defer s.wg.Done()
+		<-ctx.Done()
+		s.l.Close()
+	}()
 	return nil
 }
 
 func (s *Server) ListenAddr() string {
-	/* implement me */
-	return ""
+	return s.l.Addr().String()
 }
 
 func (s *Server) Stop() {
-	/* implement me */
+	s.stop()
+	s.wg.Wait()
+}
+
+func (s *Server) ParallelHash(ctx context.Context, req *parhashpb.ParHashReq) (resp *parhashpb.ParHashResp, err error) {
+	connections := make([]*grpc.ClientConn, len(s.conf.BackendAddrs))
+	clients := make([]hashpb.HashSvcClient, len(s.conf.BackendAddrs))
+	for i := range connections {
+		connections[i], err = grpc.Dial(s.conf.BackendAddrs[i], grpc.WithInsecure())
+		if err != nil {
+			log.Fatalf("failed to connect to %q: %v", s.conf.BackendAddrs[i], err)
+		}
+		defer connections[i].Close()
+		clients[i] = hashpb.NewHashSvcClient(connections[i])
+	}
+	var (
+		wg     = workgroup.New(workgroup.Config{Sem: s.sem})
+		hashes = make([][]byte, len(req.Data))
+	)
+	for i := range req.Data {
+		wg.Go(ctx, func(ctx context.Context) error {
+			s.lock.Lock()
+			nr := s.current
+			s.current += 1
+			s.lock.Unlock()
+			resp, err := clients[nr].Hash(ctx, &hashpb.HashReq{Data: req.Data[i]})
+			if err != nil {
+				return err
+			}
+			s.lock.Lock()
+			hashes[i] = resp.Hash
+			s.lock.Unlock()
+			return nil
+		})
+	}
+	if err := wg.Wait(); err != nil {
+		log.Fatalf("failed to hash data: %v", err)
+	}
+	return &parhashpb.ParHashResp{Hashes: hashes}, nil
 }
